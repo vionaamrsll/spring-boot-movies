b7e0c0cb7fb1fa19d0bbdccb821a8912b2d5d882
vionaamrsll <vionamarseliaaa13@gmail.com>
2024-01-06 16:30:36 +0700

    serve index html

--- src/main/java/com/movies/MoviesApplication.java
+++ src/main/java/com/movies/MoviesApplication.java
@@ -2,12 +2,18 @@ package com.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class MoviesApplication {
+public class MoviesApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("index.html");
 	}
 
 }
